fde7192dd58ca17fbba5058657c60457b333df6f
Abdelali Eljaouhari <eljaouhariabdelali@outlook.com>
2021-01-04 21:30:22 -0600

    File Handling project

---
+++ src/filehandling/ReadFileIntoList.java
@@ -0,0 +1,29 @@
+package filehandling;
+
+import java.util.*; 
+import java.nio.charset.StandardCharsets; 
+import java.nio.file.*; 
+import java.io.*; 
+
+
+public class ReadFileIntoList {
+	
+		public static List<String> readFileIntoList(String fileName) {
+			List<String> lines = Collections.emptyList();
+			try {
+				lines = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+			return lines;
+		}
+		public static void main(String[] args) { 
+			List<String> l = readFileIntoList("/Users/ali/Documents/eclipse-workspace/filehandling/test.txt"); 
+		  
+		    Iterator<String> itr = l.iterator(); 
+		    while (itr.hasNext()) 
+		      System.out.println(itr.next()); 
+		} 
+
+}
