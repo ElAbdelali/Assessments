d6316b957b25d31fd3f663aee321736c73e9a128
Ali Eljaouhari <eljaouhariabdelali@outlook.com>
2021-01-08 18:14:24 -0600

    Longest Increasing Subsequence

---
+++ src/lis/LongestSubSequence.java
@@ -0,0 +1,17 @@
+package lis;
+
+
+public class LongestSubSequence {
+	
+	public static void main(String[] args) {
+	        LongestIncreasingSubsequence longestIncreasingSubsequence = new LongestIncreasingSubsequence();
+
+	        Integer[] result = longestIncreasingSubsequence.getLongestIncreasingSubsequence();
+
+	        System.out.println("Winning sequence is");
+	        for (Integer i : result) {
+	            System.out.println("number: " + i);
+	        }
+	        System.out.println("End of sequence");
+		    }
+	}
